09a9544b286c860f12e77f96e8046ec963e45374
MERKATOR <MERKATOR@DESKTOP-IKG681A>
2019-01-16 15:41:42 +0300

    Implemented IntegrationTests;

--- src/CinemaSchedule.IntegrationTests/Api/CinemaControllerTests.cs
+++ src/CinemaSchedule.IntegrationTests/Api/CinemaControllerTests.cs
@@ -20,5 +20,41 @@ namespace CinemaSchedule.IntegrationTests.Api
 				Assert.True(cinemas.Length > 0);
 			}
 		}
+
+		[Fact]
+		public async Task GetAllMoviesTest()
+		{
+			using(var factory = new CustomWebApplicationFactory())
+			{
+				var client = factory.CreateClient();
+				var response = await client.GetAsync("/api/Cinema/GetAllMovies");
+
+				response.EnsureSuccessStatusCode();
+				var movies = await response.DeserializeAsync<MovieAm[]>();
+
+				Assert.True(movies.Length > 0);
+			}
+		}
+
+		[Fact]
+		public async Task GetMoviesByCinemaIdTest()
+		{
+			using(var factory = new CustomWebApplicationFactory())
+			{
+				var client = factory.CreateClient();
+
+				var cinemasResponse = await client.GetAsync("/api/Cinema/GetAllCinemas");
+
+				cinemasResponse.EnsureSuccessStatusCode();
+				var cinemas = await cinemasResponse.DeserializeAsync<CinemaAm[]>();
+
+				var moviesResponse = await client.GetAsync($"/api/Cinema/GetMoviesByCinemaId?cinemaId={cinemas[0].Id}");
+
+				moviesResponse.EnsureSuccessStatusCode();
+				var movies = await moviesResponse.DeserializeAsync<MovieAm[]>();
+
+				Assert.True(movies.Length > 0);
+			}
+		}
 	}
 }
---
+++ src/CinemaSchedule.IntegrationTests/Site/HomeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CinemaSchedule.IntegrationTests.Site
+{
+	public class HomeControllerTests
+	{
+		[Fact]
+		public async Task ScheduleEditorTest()
+		{
+			using(var factory = new CustomWebApplicationFactory())
+			{
+				var client = factory.CreateClient();
+				var response = await client.GetAsync("/Home/ScheduleEditor");
+
+				response.EnsureSuccessStatusCode();
+
+				Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+			}
+		}
+	}
+}
